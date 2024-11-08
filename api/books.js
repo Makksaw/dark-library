8bcf727b42178bd4b23fcc0dc6b6c20726e303e6
Makksaw <maxim.razumovsky@icloud.com>
2024-11-08 06:12:46 +0200

    Add api/books tab

---
+++ api/books.js
@@ -0,0 +1,9 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export default async function handler(req, res) {
+    const filePath = path.join(process.cwd(), 'public', 'db.json');
+    const jsonData = await fs.readFile(filePath, 'utf8');
+    const data = JSON.parse(jsonData);
+    res.status(200).json(data.books);
+}
